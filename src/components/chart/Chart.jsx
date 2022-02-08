a0bc57796c87b0b2c680d5c07317322899c4cf71
alejandropascuale <apascuale@outlook.com>
2022-02-08 12:51:40 -0300

    create chart folder and Chart component

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,9 @@
+import './Chart.css'
+
+export default function Chart() {
+  return (
+    <div className='chart'>
+      Chart
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import Chart from '../../components/chart/Chart'
 import './Home.css'
 
 export default function Home() {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart />
     </div>
   )
 }
