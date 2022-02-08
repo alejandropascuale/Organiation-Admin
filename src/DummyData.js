2e0c277f1b3160ed2970e2cee388329d344ad2c0
alejandropascuale <apascuale@outlook.com>
2022-02-08 15:36:54 -0300

    add props to chart component to reuse same

---
+++ src/DummyData.js
@@ -0,0 +1,74 @@
+export const userData = [
+  {
+    name: 'Ene',
+    'Active User': 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Feb',
+    'Active User': 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Mar',
+    'Active User': 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Apr',
+    'Active User': 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'May',
+    'Active User': 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Jun',
+    'Active User': 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Jul',
+    'Active User': 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Ago',
+    'Active User': 2200,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Sep',
+    'Active User': 960,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Oct',
+    'Active User': 5100,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Nov',
+    'Active User': 4750,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Dic',
+    'Active User': 2980,
+    pv: 4300,
+    amt: 2100,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
 import './Home.css'
+import { userData } from '../../DummyData';
 
 export default function Home() {
+
   return (
     <div className='home'>
       <FeaturedInfo />
-      <Chart />
+      <Chart
+        title={'User Analytics'}
+        data={userData}
+        grid
+        dataKey={'Active User'}
+      />
     </div>
   )
 }
