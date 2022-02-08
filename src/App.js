18a0cf9f5ae94d6f6a240b599e301d0b94e7457d
alejandropascuale <apascuale@outlook.com>
2022-02-08 10:50:13 -0300

    create sideBar and add main access buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import './App.css'
+import Sidebar from "./components/Sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <div className="container">
+        <Sidebar />
+        <div className='others'>
+          others pages
+        </div>
+      </div>
     </div>
   );
 }
