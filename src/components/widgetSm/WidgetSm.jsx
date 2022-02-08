40f3c35b94f1b12944ee79422a8a68ee183dd1a6
alejandropascuale <apascuale@outlook.com>
2022-02-08 16:19:10 -0300

    add sample data and format in widgetSm

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,9 +1,82 @@
 import './WidgetSm.css'
+import { Visibility } from '@material-ui/icons'
 
 export default function WidgetSm() {
   return (
     <div className='widgetSm'>
-      WidgetSm
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+          <li className="widgetSmListItem">
+              <img 
+                src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2018-04/shutterstockyulia_mayorova.jpg?itok=Kkr_Nqk5" 
+                alt="" 
+                className="widgetSmImg" />
+              <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Anna Keller</span>
+                  <span className="widgetSmUserTitle">Software Engineer</span>
+              </div>
+              <button className="widgetSmButton">
+                  <Visibility className='widgetSmIcon'/>
+                  Display
+              </button>
+          </li>
+          <li className="widgetSmListItem">
+              <img 
+                src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2018-04/shutterstockyulia_mayorova.jpg?itok=Kkr_Nqk5" 
+                alt="" 
+                className="widgetSmImg" />
+              <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Anna Keller</span>
+                  <span className="widgetSmUserTitle">Software Engineer</span>
+              </div>
+              <button className="widgetSmButton">
+                  <Visibility className='widgetSmIcon'/>
+                  Display
+              </button>
+          </li>
+          <li className="widgetSmListItem">
+              <img 
+                src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2018-04/shutterstockyulia_mayorova.jpg?itok=Kkr_Nqk5" 
+                alt="" 
+                className="widgetSmImg" />
+              <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Anna Keller</span>
+                  <span className="widgetSmUserTitle">Software Engineer</span>
+              </div>
+              <button className="widgetSmButton">
+                  <Visibility className='widgetSmIcon'/>
+                  Display
+              </button>
+          </li>
+          <li className="widgetSmListItem">
+              <img 
+                src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2018-04/shutterstockyulia_mayorova.jpg?itok=Kkr_Nqk5" 
+                alt="" 
+                className="widgetSmImg" />
+              <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Anna Keller</span>
+                  <span className="widgetSmUserTitle">Software Engineer</span>
+              </div>
+              <button className="widgetSmButton">
+                  <Visibility className='widgetSmIcon'/>
+                  Display
+              </button>
+          </li>
+          <li className="widgetSmListItem">
+              <img 
+                src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2018-04/shutterstockyulia_mayorova.jpg?itok=Kkr_Nqk5" 
+                alt="" 
+                className="widgetSmImg" />
+              <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Anna Keller</span>
+                  <span className="widgetSmUserTitle">Software Engineer</span>
+              </div>
+              <button className="widgetSmButton">
+                  <Visibility className='widgetSmIcon'/>
+                  Display
+              </button>
+          </li>
+      </ul>
     </div>
   )
 }
