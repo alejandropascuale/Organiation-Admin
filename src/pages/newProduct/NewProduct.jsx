99731752b4306dd65f9dfd313ff1b2bd2684f3c6
alejandropascuale <apascuale@outlook.com>
2022-02-11 14:37:14 -0300

    create form for add products

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -3,7 +3,29 @@ import './newProduct.css'
 export default function NewProduct() {
   return (
     <div className='newProduct'>
-      New Product
+        <h1 className="addProductTitle">New Product</h1>
+        <form action="" className="addProductForm">
+                <div className="addProductItem">
+                    <label>Image</label>
+                    <input type="file" id="file" />
+                </div>
+                <div className="addProductItem">
+                    <label>Name</label>
+                    <input type="text" placeholder='Apple Macbook Air' />
+                </div>
+                <div className="addProductItem">
+                    <label>Stock</label>
+                    <input type="number" placeholder='123' />
+                </div>
+                <div className="addProductItem">
+                    <label>Active</label>
+                    <select name="active" id="active">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <button className="productButton">Create</button>
+            </form>
     </div>
   )
 }
